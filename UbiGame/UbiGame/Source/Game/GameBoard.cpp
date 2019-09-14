6da2a4752fc468e00a3a0cd58eb16d570f13e0a8
Ian <ianw3214@gmail.com>
2019-09-14 11:23:25 -0400

    Implement a super hacky curl

--- UbiGame/UbiGame/Source/Game/GameBoard.cpp
+++ UbiGame/UbiGame/Source/Game/GameBoard.cpp
@@ -5,16 +5,26 @@
 #include "GameEngine\EntitySystem\Components\CollidableComponent.h"
 #include "GameEngine\EntitySystem\Components\CollidablePhysicsComponent.h"
 #include "GameEngine\EntitySystem\Components\SpriteRenderComponent.h"
+#include "GameEngine\Util\Network.h"
 
 #include "Game\Components\PlayerMovementComponent.h"
 
 using namespace Game;
 
+#include <iostream>
 GameBoard::GameBoard()
 	: m_player(nullptr)
 {
 	CreatePlayer();
 	CreateObstacle();
+
+	// system("curl \"https://testnet-algorand.api.purestake.io/ps1/v1/account/3ZVAYYEOZRPXD3XDNS3YRHP2CQ4RBPPUVL4HFIAOFVQXZ2TRKYBKLDMK6I\" -H \"Host: testnet-algorand.api.purestake.io\" -H \"X-API-Key: B3SU4KcVKi94Jap2VXkK83xx38bsv95K5UZm2lab\" -o test.txt");
+	std::string url = "https://testnet-algorand.api.purestake.io/ps1/v1/account/3ZVAYYEOZRPXD3XDNS3YRHP2CQ4RBPPUVL4HFIAOFVQXZ2TRKYBKLDMK6I";
+	std::vector<std::string> headers;
+	headers.emplace_back("Host: testnet-algorand.api.purestake.io");
+	headers.push_back("X-API-Key: B3SU4KcVKi94Jap2VXkK83xx38bsv95K5UZm2lab");
+	std::string result = Network::GetRequest(url, headers);
+	std::cout << result << std::endl;
 }
 
 
